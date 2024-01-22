bbe2715011ae4543ce33c9762513b2bd0aa79882
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2024-01-22 13:10:53 +0100

    WCAGARIA11

--- src/components/WCAGInfoAndRelationships/WCAGARIA11.stories.tsx
+++ src/components/WCAGInfoAndRelationships/WCAGARIA11.stories.tsx
@@ -4,23 +4,239 @@ import { Controls, Source, Stories, Subtitle, Title } from '@storybook/blocks'
 import WCAGARIA11 from './WCAGARIA11'
 
 const metaARIA11: Meta<typeof WCAGARIA11> = {
-    title: 'WCAGARIA11',
     component: WCAGARIA11,
     tags: ['autodocs'],
     parameters: {
         docs: {
             page: () => (
                 <>
-                    <Title />
+                    <Title>
+                        WCAGARIA11: Using ARIA landmarks to identify regions of
+                        a page
+                    </Title>
                     <Subtitle>
-                        You can find the WCAG2.2 documentation for this success
-                        criteria{' '}
-                        <a
-                            href="https://www.w3.org/WAI/WCAG22/Techniques/aria/ARIA11"
-                            target="_blank"
-                        >
-                            here.
-                        </a>
+                        <strong>
+                            This component provides programmatic access to
+                            sections of a web page by making use of the WAI-ARIA
+                            landmark roles.
+                        </strong>
+                        <p style={{ textAlign: 'center' }}>
+                            <strong>
+                                <i style={{ color: '#ff0000' }}>
+                                    variable name
+                                </i>{' '}
+                                means that this variable is mandatory
+                            </strong>
+                        </p>
+                        <p>
+                            In order to use this component correctly, you need
+                            to provide one of eight possible WAI-ARIA roles.
+                        </p>
+                        <div>
+                            These are the available WAI-ARIA role parameters for
+                            the attribute{' '}
+                            <strong style={{ color: '#ff0000' }}>
+                                role: string
+                            </strong>
+                            :
+                            <table
+                                style={{
+                                    margin: '2% auto',
+                                    backgroundColor: '#e9e9e9',
+                                    borderSpacing: '15px 2px',
+                                    padding: '2% 5%',
+                                }}
+                            >
+                                <tbody>
+                                    <tr>
+                                        <th
+                                            style={{
+                                                textAlign: 'left',
+                                            }}
+                                        >
+                                            name
+                                        </th>
+                                        <th style={{ textAlign: 'left' }}>
+                                            information
+                                        </th>
+                                    </tr>
+                                    <tr>
+                                        <td>banner</td>
+                                    </tr>
+                                    <tr>
+                                        <td>complementary</td>
+                                    </tr>
+                                    <tr>
+                                        <td>contentinfo</td>
+                                    </tr>
+                                    <tr>
+                                        <td>form</td>
+                                    </tr>
+                                    <tr>
+                                        <td>main</td>
+                                    </tr>
+                                    <tr>
+                                        <td>navigation</td>
+                                    </tr>
+                                    <tr>
+                                        <td>region</td>
+                                    </tr>
+                                    <tr>
+                                        <td>search</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <p>
+                            If you are selecting{' '}
+                            <strong>role = 'search'</strong>, a form element is
+                            returned. Otherwise, a div element is returned.
+                        </p>
+                        <p>
+                            As this component renders either a simple div or
+                            form element, you are also free to assign every
+                            possible property of the div or form element to the
+                            element of this component optionally. These
+                            attributes can be applied to the{' '}
+                            <strong>elementData</strong> object.
+                        </p>
+                        <p>
+                            You can pass any element as a{' '}
+                            <strong style={{ color: '#ff0000' }}>
+                                child: ReactNode
+                            </strong>{' '}
+                            element: be it a string, another div element or a
+                            completely different component.
+                        </p>
+                        <p>
+                            In order to connect the child element with its
+                            container, you can either use the{' '}
+                            <strong>ariaLabelledById</strong> or the{' '}
+                            <strong>ariaLabel</strong> parameter. If you choose
+                            the first option, you have to add an{' '}
+                            <strong>ID attribute</strong> to your child element
+                            which has the same value as the one inside the{' '}
+                            <strong>ariaLabelledById</strong> variable. If you
+                            choose the second option, the value inside the{' '}
+                            <strong>ariaLabel</strong> parameter should be
+                            descriptive enough to be able to describe your child
+                            element.
+                        </p>
+                        <p>
+                            You can also add an optional customised{' '}
+                            <strong>className: string</strong> to your
+                            component, which allows you to apply your styling to
+                            your new accessible component. If you prefer styling
+                            your components inline, feel free to use the{' '}
+                            <strong>additionalStyling</strong> variable inside
+                            of the <strong>elementData</strong> object.
+                        </p>
+                        <div>
+                            For even more accessibility, you can also use
+                            optional additional WAI-ARIA attributes. They are
+                            stored in the{' '}
+                            <strong>additionalAriaAttributes</strong> object.
+                            You can use the following WAI-ARIA attributes, but
+                            without the 'aria-' prefix. But be careful: You
+                            cannot use every WAI-ARIA attribute with every
+                            WAI-ARIA role!
+                            <table
+                                style={{
+                                    margin: '2% auto',
+                                    backgroundColor: '#e9e9e9',
+                                    padding: '2% 5%',
+                                    borderSpacing: '15px 2px',
+                                }}
+                            >
+                                <tbody>
+                                    <tr>
+                                        <th style={{ textAlign: 'left' }}>
+                                            attribute
+                                        </th>
+                                        <th style={{ textAlign: 'left' }}>
+                                            type
+                                        </th>
+                                    </tr>
+                                    <tr>
+                                        <td>aria-atomic</td>
+                                        <td>boolean</td>
+                                    </tr>
+                                    <tr>
+                                        <td>aria-busy</td>
+                                        <td>boolean</td>
+                                    </tr>
+                                    <tr>
+                                        <td>aria-controls</td>
+                                        <td>string</td>
+                                    </tr>
+                                    <tr>
+                                        <td>aria-current</td>
+                                        <td>
+                                            'page' | 'step' | 'location' |
+                                            'date' | 'time' | 'true' | 'false' |
+                                            true | false
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>aria-describedby</td>
+                                        <td>string</td>
+                                    </tr>
+                                    <tr>
+                                        <td>aria-description</td>
+                                        <td>string</td>
+                                    </tr>
+                                    <tr>
+                                        <td>aria-details</td>
+                                        <td>string</td>
+                                    </tr>
+                                    <tr>
+                                        <td>aria-flowto</td>
+                                        <td>string</td>
+                                    </tr>
+                                    <tr>
+                                        <td>aria-hidden</td>
+                                        <td>
+                                            'false' | 'true' | undefined | false
+                                            | true
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>aria-keyshortcuts</td>
+                                        <td>string</td>
+                                    </tr>
+                                    <tr>
+                                        <td>aria-live</td>
+                                        <td>'assertive' | 'off' | 'polite'</td>
+                                    </tr>
+                                    <tr>
+                                        <td>aria-owns</td>
+                                        <td>string</td>
+                                    </tr>
+                                    <tr>
+                                        <td>aria-relevant</td>
+                                        <td>
+                                            'additions' | 'all' | 'removals' |
+                                            'text' | 'additions text'
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>aria-roledescription</td>
+                                        <td>string</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <p>
+                            <a
+                                href="https://www.w3.org/WAI/WCAG22/Techniques/aria/ARIA11"
+                                target="_blank"
+                            >
+                                You can find the full World Wide Web Consortium,
+                                Inc. WCAG2.2 documentation for this success
+                                criteria here.
+                            </a>
+                        </p>
+                        <br />
                     </Subtitle>
                     <Stories />
                     <Controls />
@@ -93,7 +309,7 @@ export const AriaLabelledBy: StoryARIA11 = {
 export const AriaLabel: StoryARIA11 = {
     render: () => (
         <>
-            <WCAGARIA11 role="navigation" ariaLabel="Site">
+            <WCAGARIA11 role="navigation" ariaLabel="Home page navigation">
                 <ul>
                     <li>
                         <a href="...">nav link 1</a>
@@ -106,16 +322,16 @@ export const AriaLabel: StoryARIA11 = {
                     </li>
                 </ul>
             </WCAGARIA11>
-            <WCAGARIA11 role="navigation" ariaLabelledById="Tags">
+            <WCAGARIA11 role="navigation" ariaLabel="Home page tags">
                 <ul>
                     <li>
-                        <a href="...">nav link 1</a>
+                        <a href="...">tag 1</a>
                     </li>
                     <li>
-                        <a href="...">nav link 2</a>
+                        <a href="...">tag 2</a>
                     </li>
                     <li>
-                        <a href="...">nav link 3</a>
+                        <a href="...">tag 3</a>
                     </li>
                 </ul>
             </WCAGARIA11>
@@ -125,7 +341,7 @@ export const AriaLabel: StoryARIA11 = {
 
 export const Search: StoryARIA11 = {
     render: () => (
-        <WCAGARIA11 role="search">
+        <WCAGARIA11 role="search" ariaLabelledById="search-label">
             <>
                 <label
                     htmlFor="product-search"
--- src/components/WCAGInfoAndRelationships/WCAGARIA11.tsx
+++ src/components/WCAGInfoAndRelationships/WCAGARIA11.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react'
 import { GlobalAttributes } from '../interfaces/GlobalAttributes'
+import { WAIARIAAttributes } from '../interfaces/WAIARIAAttributes'
 
 interface WCAGARIA11Props extends GlobalAttributes {
     role:
@@ -11,18 +12,59 @@ interface WCAGARIA11Props extends GlobalAttributes {
         | 'navigation'
         | 'region'
         | 'search'
+    additionalAriaAttributes?: Omit<
+        WAIARIAAttributes,
+        | 'activedescendant'
+        | 'autocomplete'
+        | 'braillelabel'
+        | 'brailleroledescription'
+        | 'checked'
+        | 'colcount'
+        | 'colindex'
+        | 'colindextext'
+        | 'colspan'
+        | 'disabled'
+        | 'errormessage'
+        | 'expanded'
+        | 'haspopup'
+        | 'invalid'
+        | 'label'
+        | 'labelledby'
+        | 'level'
+        | 'modal'
+        | 'multiline'
+        | 'multiselectable'
+        | 'orientation'
+        | 'placeholder'
+        | 'posinset'
+        | 'pressed'
+        | 'readonly'
+        | 'required'
+        | 'rowcount'
+        | 'rowindex'
+        | 'rowindextext'
+        | 'rowspan'
+        | 'selected'
+        | 'setsize'
+        | 'sort'
+        | 'valuemax'
+        | 'valuemin'
+        | 'valuenow'
+        | 'valuetext'
+    >
     ariaLabel?: string
     ariaLabelledById?: string
-    divData?: GlobalAttributes
-    children: ReactNode
     className?: string
+    elementData?: GlobalAttributes
+    children: ReactNode
 }
 
 const WCAGARIA11: React.FC<WCAGARIA11Props> = ({
+    additionalAriaAttributes,
     ariaLabel,
     ariaLabelledById,
     className,
-    divData,
+    elementData,
     role,
     children,
 }) => {
@@ -33,8 +75,24 @@ const WCAGARIA11: React.FC<WCAGARIA11Props> = ({
                     role={role}
                     aria-label={ariaLabel}
                     aria-labelledby={ariaLabelledById}
-                    style={{ ...divData?.additionalStyling }}
+                    style={{ ...elementData?.additionalStyling }}
                     className={className}
+                    aria-atomic={additionalAriaAttributes?.atomic}
+                    aria-busy={additionalAriaAttributes?.busy}
+                    aria-controls={additionalAriaAttributes?.controls}
+                    aria-current={additionalAriaAttributes?.current}
+                    aria-describedby={additionalAriaAttributes?.describedby}
+                    aria-description={additionalAriaAttributes?.description}
+                    aria-details={additionalAriaAttributes?.details}
+                    aria-flowto={additionalAriaAttributes?.flowto}
+                    aria-hidden={additionalAriaAttributes?.hidden}
+                    aria-keyshortcuts={additionalAriaAttributes?.keyshortcuts}
+                    aria-live={additionalAriaAttributes?.live}
+                    aria-owns={additionalAriaAttributes?.owns}
+                    aria-relevant={additionalAriaAttributes?.relevant}
+                    aria-roledescription={
+                        additionalAriaAttributes?.roledescription
+                    }
                 >
                     {children}
                 </form>
@@ -43,20 +101,35 @@ const WCAGARIA11: React.FC<WCAGARIA11Props> = ({
                     role={role}
                     aria-label={ariaLabel}
                     aria-labelledby={ariaLabelledById}
-                    style={{ ...divData?.additionalStyling }}
+                    style={{ ...elementData?.additionalStyling }}
                     className={className}
-                    accessKey={divData?.accesskey}
-                    contentEditable={divData?.contenteditable}
-                    dir={divData?.dir}
-                    draggable={divData?.draggable}
-                    hidden={divData?.hiddenElement}
-                    id={divData?.id}
-                    inputMode={divData?.inputmode}
-                    lang={divData?.lang}
-                    spellCheck={divData?.spellcheck}
-                    tabIndex={divData?.tabindex}
-                    title={divData?.title}
-                    translate={divData?.translate}
+                    accessKey={elementData?.accesskey}
+                    contentEditable={elementData?.contenteditable}
+                    dir={elementData?.dir}
+                    draggable={elementData?.draggable}
+                    hidden={elementData?.hiddenElement}
+                    id={elementData?.id}
+                    inputMode={elementData?.inputmode}
+                    lang={elementData?.lang}
+                    spellCheck={elementData?.spellcheck}
+                    tabIndex={elementData?.tabindex}
+                    title={elementData?.title}
+                    translate={elementData?.translate}
+                    aria-atomic={additionalAriaAttributes?.atomic}
+                    aria-busy={additionalAriaAttributes?.busy}
+                    aria-controls={additionalAriaAttributes?.controls}
+                    aria-current={additionalAriaAttributes?.current}
+                    aria-describedby={additionalAriaAttributes?.describedby}
+                    aria-description={additionalAriaAttributes?.description}
+                    aria-details={additionalAriaAttributes?.details}
+                    aria-flowto={additionalAriaAttributes?.flowto}
+                    aria-hidden={additionalAriaAttributes?.hidden}
+                    aria-live={additionalAriaAttributes?.live}
+                    aria-owns={additionalAriaAttributes?.owns}
+                    aria-relevant={additionalAriaAttributes?.relevant}
+                    aria-roledescription={
+                        additionalAriaAttributes?.roledescription
+                    }
                 >
                     {children}
                 </div>
